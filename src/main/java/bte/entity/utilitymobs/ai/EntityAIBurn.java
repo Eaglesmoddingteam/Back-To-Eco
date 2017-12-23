2112857b6be3698a0fb1553a68dbde3f36697f56
Tebreca <ewan.arends10@gmail.com>
2017-12-23 18:24:43 +0100

    burn ai update

--- src/main/java/bte/entity/utilitymobs/ai/EntityAIBurn.java
+++ src/main/java/bte/entity/utilitymobs/ai/EntityAIBurn.java
@@ -15,6 +15,7 @@ import net.minecraft.util.math.Vec3d;
 public class EntityAIBurn extends EntityAIBase {
 	EntityCreature creatureIn;
 	EntityUtilMob mobIn;
+	int tick = 0;
 	
 	public EntityAIBurn(EntityCreature creatureIn) {
 		this.creatureIn=creatureIn;
@@ -28,20 +29,42 @@ public class EntityAIBurn extends EntityAIBase {
 	}
 	
 	@Override
+	public boolean shouldContinueExecuting() {
+		return true;
+	}
+	
+	@Override
 	public void startExecuting() {
 		Main.LOGGER.warn("Burned Yes Baby");
 		List<EntityItem> near = creatureIn.world.getEntitiesWithinAABB(EntityItem.class, creatureIn.getEntityBoundingBox().expand(4, 1, 4));
 		for (EntityItem item : near) {
-			creatureIn.faceEntity(item, 700f, 700f);
-			item.world.spawnEntity(BurnHelper.burn(item.getItem(), item.posX, item.posY, item.posZ, item.world));
-			item.world.spawnParticle(EnumParticleTypes.FLAME, creatureIn.posX, creatureIn.posY, creatureIn.posZ, 1, 1, 1, 10, 10);
+			EntityItem item2 = BurnHelper.burn(item.getItem(), item.posX, item.posY, item.posZ, item.world);
+			if(item2 != null) {
+			item.world.spawnEntity(item2);
+			item.world.spawnParticle(EnumParticleTypes.FLAME, creatureIn.posX, creatureIn.posY, creatureIn.posZ, 1, 1, 1);
 			item.setDead();
+			}
 		}
+	}
+	
+	@Override
+	public void resetTask() {
+		tick = 0;
+		super.resetTask();
 	}
 	
 	private AxisAlignedBB calcItems() {
 		AxisAlignedBB aabb = new AxisAlignedBB(creatureIn.getPositionVector(), creatureIn.getLookVec());
 		return aabb;
+	}
+	
+	@Override
+	public void updateTask() {
+		tick++;
+		if(tick >= 100) {
+			resetTask();
+		startExecuting();	
+		}
 	}
 
 }
--- src/main/java/bte/util/entityhelper/BurnHelper.java
+++ src/main/java/bte/util/entityhelper/BurnHelper.java
@@ -1,38 +1,34 @@
 package bte.util.entityhelper;
 
+import java.util.Map;
+
 import akka.actor.IndirectActorProducer;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.registries.ForgeRegistry;
 
 public class BurnHelper {
 	
+	static Map<ItemStack, ItemStack> recipes;
+	
+	public static void InitRecipes() {
+	recipes = FurnaceRecipes.instance().getSmeltingList();	
+	}
+	
 	public static EntityItem burn(ItemStack input, double x, double y, double z, World world) {
-		Item item = input.getItem();
-		int amount = input.getCount();
-		if(item == item.getItemFromBlock(Blocks.IRON_ORE)) {
-			EntityItem output = new EntityItem(world, x, y, z, new ItemStack(Items.IRON_INGOT, amount * (int) (Math.random() + 2)));
+		if(recipes.containsKey(input)) {
+			ItemStack out = recipes.get(input);
+			out.setCount(out.getCount() + (int)((Math.random() * 2) * out.getCount() + 1));
+			EntityItem output = new EntityItem(world, x, y, z, out);
 			return output;
 		}
-		
-		if(item == item.getItemFromBlock(Blocks.GOLD_ORE)) {
-			EntityItem output = new EntityItem(world, x, y, z, new ItemStack(Items.GOLD_INGOT, amount * (int) (Math.random() + 2)));
-			return output;
-		}
-		
-		if(item == item.getItemFromBlock(Blocks.DIAMOND_ORE)) {
-			EntityItem output = new EntityItem(world, x, y, z, new ItemStack(Items.DIAMOND, amount * (int) (Math.random() + 2)));
-			return output;
-		}
-		
-		if(item == item.getItemFromBlock(Blocks.REDSTONE_ORE)) {
-			EntityItem output = new EntityItem(world, x, y, z, new ItemStack(Items.REDSTONE, amount * (int) (Math.random() * 13 + 12)));
-			return output;
-		}
-		
 		return null;
 	}
 	
